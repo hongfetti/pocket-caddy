62d0061776b19520215657bc46d52cf866dcdb44
hongfetti <ryan.hong1994@gmail.com>
2025-07-03 01:19:15 -0400

    worked on profile card - have score working - still want to add some things to it

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -1,22 +1,53 @@
-import "./Profile.css"
-import auth from "../../utils/auth.ts";
+import "./Profile.css";
+
+import { useQuery } from "@apollo/client";
+import { GET_CURRENT_USER } from "../../utils/queries.ts";
+
+type Score = {
+    _id: string;
+    courseName: string;
+    par: number;
+    totalScore: number
+}
 
 const Profile = () => {
-    const profile = auth.getProfile();
-    const currentUser = profile?.data
+  const { loading, error, data } = useQuery(GET_CURRENT_USER);
 
-    console.log(currentUser)
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error loading profile: {error.message}</p>;
 
-    return currentUser ? (
-        <div>
-            <h2>Welcome, {currentUser.name}</h2>
-            <div className="card">
-                <div>Card Content</div>
-            </div>
-        </div>
-    ) : (
-        <div></div> // have to add conditional as getting ts error for currentUser possible null
-    )
+  const currentUser = data?.currentUser;
+  console.log(currentUser);
+
+  const scores = currentUser?.scores;
+
+  const bestScore = scores?.length
+    ? scores.reduce((best: Score, current: Score) => {
+        const currentDiff = current.totalScore - current.par;
+        const bestDiff = best.totalScore - best.par;
+
+        return currentDiff < bestDiff ? current : best;
+    }, scores[0])
+    : null
+
+  return (
+    <main>
+      <h2>Welcome, {currentUser.name}</h2>
+      <div className="card" id="main-card">
+        {bestScore? (
+          <div>
+        <p><strong>Best Score:</strong> {bestScore.totalScore}</p>
+        <p><strong>Course:</strong> {bestScore.courseName}</p>
+        <p><strong>Score vs Par:</strong> {bestScore.totalScore - bestScore.par > 0
+          ? `+${bestScore.totalScore - bestScore.par}`
+          : bestScore.totalScore - bestScore.par}</p>
+      </div>
+        ) : (
+          <p>No scores yet</p>
+        )}
+      </div>
+    </main>
+  );
 };
 
-export default Profile
+export default Profile;
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,41 +1,20 @@
-import "./Home.css"
-import { useQuery } from "@apollo/client";
-import { GET_CURRENT_USER } from "../../utils/queries.ts";
+import "./Home.css";
 import auth from "../../utils/auth.ts";
 import Login from "../../components/Login/Login.tsx";
-import Profile from "../../components/Profile/Profile.tsx"
+import Profile from "../../components/Profile/Profile.tsx";
 
 const Home = () => {
-
   const loggedIn = auth.loggedIn();
-  const { loading, error } = useQuery(GET_CURRENT_USER, {
-    skip: !loggedIn,
-    fetchPolicy: "network-only"
-  });
 
-  if (!loggedIn) {
-    return (
-    <div>
-      <main id="home-login">
-        <h1>Log in to get started!</h1>
-        <Login />
-      </main>
-    </div>
-  );
-}
-
-  if (loading) return <p>Loading user info...</p>;
-  if (error) return <p>Error fetching user: {error.message}</p>;
-  
-  return (
-    <div>
-      <main>
-        <h1>
-          Home (logged in)
-          <Profile />
-        </h1>
-      </main>
-    </div>
+  return loggedIn ? (
+    <main>
+      <Profile />
+    </main>
+  ) : (
+    <main>
+      <h1>Log in to get started!</h1>
+      <Login />
+    </main>
   );
 };
 
